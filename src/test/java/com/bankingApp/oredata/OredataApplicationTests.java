5bf0d674b21bdea8d92c637d3031d1e75135ce8e
cansinbayir <cansinb@alumni.sabanciuniv.edu>
2024-03-27 16:50:01 +0300

    2-project init

---
+++ src/test/java/com/bankingApp/oredata/OredataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingApp.oredata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OredataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
